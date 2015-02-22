049c4333dcb9f8011882a7022c9d94dfd33cf103
juren18 <juren@email.arizona.edu>
2015-02-22 12:26:45 -0700

    Ran the combined.sh script

--- combine.sh
+++ combine.sh
@@ -7,8 +7,10 @@
 OUTPUT_FILE="output/combined_gapMinder.tsv"
 
 echo Building combined country data file
+# first add column headers to output file
 cat data/clean/country.cc.txt > $OUTPUT_FILE
 
+#change name of country.cc to cc.processed so that it won't be processsed in the next command
 mv -v data/clean/country.cc.txt data/clean/country.cc.processed
 sleep 2
 
@@ -16,5 +18,6 @@ cat data/clean/*.cc.txt
 sleep 2
 cat data/clean/*.cc.txt >> $OUTPUT_FILE
 
+#change name back
 mv -v data/clean/country.cc.processed data/clean/country.cc.txt
 
