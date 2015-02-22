5a8ff008c2018dbb352f35919fcc98b9522a4309
juren18 <juren@email.arizona.edu>
2015-02-22 14:00:11 -0700

    installed ggplot, plyr, ggthemes, reshape2

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -1,3 +1,8 @@
 # ggplot learning code
 # Jana U'Ren
 # 22mar2015
+
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
